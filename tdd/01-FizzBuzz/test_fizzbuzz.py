b5b58883f726fde41b8b680175f5aef6e215197e
ivanovkirilg <ivanovkirilg@gmail.com>
2023-11-01 09:55:08 +0200

    01 test req 2

--- tdd/01-FizzBuzz/test_fizzbuzz.py
+++ tdd/01-FizzBuzz/test_fizzbuzz.py
@@ -2,14 +2,23 @@ import unittest
 
 from fizzbuzz import fizzBuzz
 
-# Write a fizzBuzz method that accepts a number as input and returns it as a String.
-
 class TestFizzBuzz(unittest.TestCase):
     def test_fizzBuzz_returns_number_as_string(self):
+        '''Write a fizzBuzz method that accepts a number as input
+           and returns it as a String.'''
         number = 2
 
         result = fizzBuzz(number)
 
         self.assertEqual(result, '2')
+
+    def test_fizzBuzz_returns_fizz_for_multiples_of_three(self):
+        '''For multiples of three return “Fizz” instead of the number'''
+        number = 3
+
+        result = fizzBuzz(number)
+
+        self.assertEqual(result, 'Fizz')
+
 
 unittest.main()
