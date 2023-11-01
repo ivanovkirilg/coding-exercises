e62a74df3e84bf0afe2b8f5300525252c23a090f
ivanovkirilg <ivanovkirilg@gmail.com>
2023-11-01 10:05:24 +0200

    01 refactor req 3

--- tdd/01-FizzBuzz/fizzbuzz.py
+++ tdd/01-FizzBuzz/fizzbuzz.py
@@ -1,9 +1,15 @@
-def _is_divisible_by(number, denominator):
-    return number % denominator == 0
+def _is_divisible_by(number, divisor):
+    return number % divisor == 0
+
+FIZZ_DIVISOR = 3
+BUZZ_DIVISOR = 5
+
+FIZZ_STRING = 'Fizz'
+BUZZ_STRING = 'Buzz'
 
 def fizzBuzz(number):
-    if _is_divisible_by(number, 3):
+    if _is_divisible_by(number, FIZZ_DIVISOR):
         return 'Fizz'
-    if _is_divisible_by(number, 5):
+    if _is_divisible_by(number, BUZZ_DIVISOR):
         return 'Buzz'
     return str(number)
