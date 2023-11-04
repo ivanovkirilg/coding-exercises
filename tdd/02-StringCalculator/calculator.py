447e687a24a88a656e5236163b8c3e8d91876301
ivanovkirilg <ivanovkirilg@gmail.com>
2023-11-04 21:42:13 +0200

    02 refactor req 4

--- tdd/02-StringCalculator/calculator.py
+++ tdd/02-StringCalculator/calculator.py
@@ -9,6 +9,13 @@ def _flatten(nested_lists):
         flat_list.extend(el)
     return flat_list
 
+def _convert_to_ints(number_strings):
+    try:
+        return list( map(int, number_strings) )
+    except ValueError as exc:
+        raise ValueError("found extraneous separator") from exc
+
+
 def add(numbers):
     if not numbers:
         return 0
@@ -18,9 +25,4 @@ def add(numbers):
     for sep in SEPARATORS:
         numbers = _flatten( num.split(sep) for num in numbers )
 
-    try:
-        numbers = [ int(num) for num in numbers ]
-    except ValueError as exc:
-        raise ValueError("found extraneous separator") from exc
-
-    return sum(numbers)
+    return sum(_convert_to_ints(numbers))
