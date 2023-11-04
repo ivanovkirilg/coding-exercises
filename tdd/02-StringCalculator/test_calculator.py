e346f0b70faf6218ff7df14a94906e5057f65507
ivanovkirilg <ivanovkirilg@gmail.com>
2023-11-04 19:24:39 +0200

    02 test req 1b

--- tdd/02-StringCalculator/test_calculator.py
+++ tdd/02-StringCalculator/test_calculator.py
@@ -13,6 +13,14 @@ class TestCalculatorAdd(unittest.TestCase):
         result = add(numbers)
         self.assertEqual(result, 0)
 
+    def test_given_one_number_then_add_returns_the_number(self):
+        '''The method can take up to two numbers, separated by commas,
+           and will return their sum as a result. So the inputs can be:
+           “”, “1”, “1,2”. For an empty string, it will return 0.'''
+        numbers = "3"
+        result = add(numbers)
+        self.assertEqual(result, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
