3b8cdafef1544916101267e76f7b92b011657b9d
ivanovkirilg <ivanovkirilg@gmail.com>
2023-12-15 19:32:37 +0200

    04 test req 3

--- tdd/04-CartTotal/cart.py
+++ tdd/04-CartTotal/cart.py
@@ -4,7 +4,12 @@ from dataclasses import dataclass
 class Product:
     price: float
 
+@dataclass
+class Coupon:
+    value: float
 
-def calculate_total(products, coupon=0.0):
+
+def calculate_total(products, coupon=Coupon(0.0)):
     total = sum( prod.price for prod in products )
-    return total - coupon
+    reduced_total = total - coupon.value
+    return reduced_total
--- tdd/04-CartTotal/test_cart.py
+++ tdd/04-CartTotal/test_cart.py
@@ -1,16 +1,4 @@
-"""
-3. If the coupon's value is greater than the summed price,
-   the function should return a 0 total and indicate
-   that the user should receive a new coupon worth the difference.
-4. Allow multiple coupons per cart.
-5. Some of the products in a user's cart may have an associated discount
-   (in percentage). Apply discounts before coupons.
-6. Allow multiple discounts per product.
-   A product with an initial price of $10
-   and two 50% discounts should total $2.50, not $0.
-"""
-
-from cart import Product, calculate_total
+from cart import Product, Coupon, calculate_total
 
 class TestCalculateTotal:
     def test_returns_sum_of_prices(self):
@@ -25,9 +13,31 @@ class TestCalculateTotal:
         ''' 2. The user may optionally use a coupon,
         in which case its value should be deducted from the total. '''
         products = [Product(0.5), Product(1), Product(1.5)]
-        coupon = 1.5
+        coupon = Coupon(1.5)
 
         total = calculate_total(products, coupon=coupon)
 
         assert total == 1.5
+
+    def test_unspent_part_of_coupon_returned(self):
+        ''' 3. If the coupon's value is greater than the summed price,
+        the function should return a 0 total and indicate
+        that the user should receive a new coupon worth the difference.
+        '''
+        products = [Product(0.5), Product(1), Product(1.5)]
+        coupon = Coupon(4)
+
+        total = calculate_total(products, coupon=coupon)
+
+        assert total == Coupon(1)
+
+
+"""
+4. Allow multiple coupons per cart.
+5. Some of the products in a user's cart may have an associated discount
+   (in percentage). Apply discounts before coupons.
+6. Allow multiple discounts per product.
+   A product with an initial price of $10
+   and two 50% discounts should total $2.50, not $0.
+"""
 
