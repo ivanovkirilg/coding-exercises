d959f4dbb4059a95bdda84a2ebef912f31f43c5a
ivanovkirilg <ivanovkirilg@gmail.com>
2023-12-15 19:26:50 +0200

    04 impl req 2

--- tdd/04-CartTotal/cart.py
+++ tdd/04-CartTotal/cart.py
@@ -5,5 +5,5 @@ class Product:
     price: float
 
 
-def calculate_total(products):
-    return sum( prod.price for prod in products )
+def calculate_total(products, coupon=0.0):
+    return sum( prod.price for prod in products ) - coupon
