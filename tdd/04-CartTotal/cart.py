f9291e96e16c7f5c3a7538ade0ccc1eb6926ebdd
ivanovkirilg <ivanovkirilg@gmail.com>
2023-12-20 10:14:13 +0200

    04 impl req 5

--- tdd/04-CartTotal/cart.py
+++ tdd/04-CartTotal/cart.py
@@ -13,8 +13,11 @@ class Coupon:
 def _sum_coupons(coupons: "list[Coupon]"):
     return sum(coupon.value for coupon in coupons)
 
+def _get_discounted_price(product):
+    return product.price * (1.0 - product.discount)
+
 def calculate_total(products, coupons: "list[Coupon]" = []):
-    total = sum( prod.price for prod in products )
+    total = sum( _get_discounted_price(prod) for prod in products )
 
     total -= _sum_coupons(coupons)
 
